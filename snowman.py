861fc4680f3fd0c71dee4943a8a015a6f9e190e9
Sarah <sarah.alhuriz@hotmail.com>
2021-01-14 16:19:45 -0700

    complete game

--- snowman.py
+++ snowman.py
@@ -8,25 +8,25 @@ continue_play = True
 class Word():
     def __init__(self, chosen_word):
         self.chosen_word = word
-    while continue_play == True:
+    while continue_play==True:
         win = False
         strikes = []
-        guesses = []
+        guesses = []    
         word = list(choice(words_arr))
-        display_word = len(word)*['*']
-        print("\nLet's play SNOWMAN! \nCan you guess the word in 8 strikes or less? \n" )
+        display_word = len(word)*['*']  
+        print("\nLet's play SNOWMAN! \nCan you guess the word in 8 strikes or less? \n")
         print("Play Word: ", "".join(display_word) + "\n")
-
+        
         while (win == False and len(strikes) < 8):
             letter = input("Guess a letter: ").lower()
 
-            if not letter in string.ascii_lowercase or letter == '':
+            if not letter in string.ascii_lowercase or letter=='':
                 continue
 
             if letter in guesses:
                 print("\nYou already guessed that letter, try again \n")
-            else:
-                guesses +=[letter]
+            else: 
+                guesses += [letter]
                 if letter in word:
                     print("\n>*>*>    Correct!    <*<*<    Keep Going.. \n")
                     for x in range(0, len(word)):
@@ -40,16 +40,17 @@ class Word():
             print("Remaining Strikes: " + str(8-len(strikes)) + "\n")
 
             if display_word == word:
-                win == True
-        
+                win = True
+
         if win == True:
             print("\nRemarkable! You Won!")
         else:
             print("\nBetter Luck Next Time!")
         print("The word was '", ''.join(word) , "'")
 
+
         while True:
-            play_again = input("\nPlay Again? y/n ")
+            play_again = input("\nPlay Again? y/n ? ")
             if play_again == 'n' or play_again == 'N':
                 print('ta ta!')
                 continue_play = False
